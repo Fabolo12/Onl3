63e151a2f04ec79ac8d7e909f60dee552d755682
valerii.popov <fabolo1234@gmail.com>
2022-10-26 20:35:18 +0300

    first commit

---
+++ src/main/java/com/popov/Main.java
@@ -0,0 +1,9 @@
+package com.popov;
+
+public class Main {
+    public static void main(String[] args) {
+        if ((true || false && true || true) && false) {
+
+        }
+    }
+}
---
+++ src/main/java/com/popov/Main2.java
@@ -0,0 +1,9 @@
+package com.popov;
+
+public class Main2 {
+    public static void main(String[] args) {
+        for (int y = 5, i = 0; i <= 10; i++, y += 2) {
+            System.out.println("Step " + i + " value " + y);
+        }
+    }
+}
